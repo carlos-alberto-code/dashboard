b6dfde91e9dd508647dddae8c00c8ebce0d19ba1
Carlos Alberto Baltazar <cabaltazar@grupobmv.com.mx>
2025-11-14 14:03:17 -0600

    Actualización de la URL base de la API y ajustes en la configuración de TypeScript

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import './index.css'
+import './index.css' 
 import React from 'react'
 import App from './App.tsx'
 import {createRoot} from 'react-dom/client'
--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import React, {useState} from 'react';
 import {login} from '../api/login.ts';
 import {useNavigate} from 'react-router-dom';
 import type {LoginCredentials} from '../types/login.ts';
