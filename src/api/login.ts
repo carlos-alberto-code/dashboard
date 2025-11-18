890eb76ac07e855d645773fd767832d0902d87ff
Carlos Alberto Baltazar <cabaltazar@grupobmv.com.mx>
2025-11-18 17:09:57 -0600

    Mejora en la gestión de la respuesta de login, asignando datos de la API a una variable

--- src/api/login.ts
+++ src/api/login.ts
@@ -2,7 +2,8 @@ import api from "./api.ts";
 import type {LoginCredentials, LoginRespuesta} from "../types/login.ts";
 
 export const login = async (credentials: LoginCredentials): Promise<LoginRespuesta> => {
-    const {loginData}: LoginCredentials = await api.post<LoginRespuesta>("/auth/login", credentials);
+    const response = await api.post<LoginRespuesta>("/auth/login", credentials);
+    const loginData: LoginRespuesta = response.data;
     localStorage.setItem("access_token", loginData.access_token);
     return loginData;
 };
