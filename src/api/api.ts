26b0af4bc1aef0e747b73c622cef43db9f555b48
Carlos Alberto Baltazar <cabaltazar@grupobmv.com.mx>
2025-11-18 18:05:06 -0600

    Implementación de configuración para localhost y pre-producción. Ahora es necesario usar comandos diferentes para que apunte a un ambiente diferente.

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
+const baseURL = (import.meta as any).env?.VITE_API_BASE_URL ?? 'https://backend-ruta-lince.onrender.com/api';
+
 const api = axios.create({
-    // baseURL: 'http://127.0.0.1:8081/api',
-    baseURL: 'https://backend-ruta-lince.onrender.com/api',
+    baseURL,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
