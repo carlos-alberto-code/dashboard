a1abc46af6bcb22fbce42adadce0d2d8002bf5a6
Carlos Alberto Baltazar <cabaltazar@grupobmv.com.mx>
2025-11-27 17:43:52 -0600

    Se modificó una letra de la ruta del endpoint

--- src/controllers/GamificationController.tsx
+++ src/controllers/GamificationController.tsx
@@ -1,14 +1,14 @@
 import api from '../api/api';
-import type { GamificationData } from '../types/Gamification';
+import type {GamificationData} from '../types/Gamification';
 
 export class GamificationController {
-  static async getGamificationData(): Promise<GamificationData> {
-    try {
-      const response = await api.get('/gamification/datos');
-      return response.data;
-    } catch (error) {
-      console.error('Error fetching learning data:', error);
-      throw error;
+    static async getGamificationData(): Promise<GamificationData> {
+        try {
+            const response = await api.get('/gamificacion/datos');
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching learning data:', error);
+            throw error;
+        }
     }
-  }
 }
